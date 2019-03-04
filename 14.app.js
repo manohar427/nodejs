0e08a316ea8e90316a5145f0072d9370149ee0be
Manohar Sambayyapalem <manohar427@gmail.com>
2019-03-04 18:06:50 +0530

    filters

---
+++ 14.app.js
@@ -0,0 +1,24 @@
+var http = require('http');
+var fs = require('fs');
+
+
+
+var http = require('http');
+var server = http.createServer(function(req,res) {
+
+console.log('Request:'+req.url);
+res.writeHead(200,{'Content-Type':'application/json'});
+var home = {'name':'Home'};
+var contact = {'name':'Contact'};
+
+if(req.url === '/' || req.url === '/home'){
+  res.end(JSON.stringify(home));
+}else if(req.url === '/contact') {
+  res.end(JSON.stringify(contact));
+}else{
+  res.end(JSON.stringify("Not Found"));
+}
+
+});
+
+server.listen(3091);
